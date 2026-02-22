82175b7de536695b6f4295b8fea8161506b795e9
SantiagoSu15 <sasupu08@gmail.com>
2026-02-21 20:20:22 -0500

    edit agente

--- agent.py
+++ agent.py
@@ -48,13 +48,10 @@ model = init_chat_model(
 
 
 
-# We use a dataclass here, but Pydantic models are also supported.
 @dataclass
 class ResponseFormat:
     """Response schema for the agent."""
-    # A punny response (always required)
     punny_response: str
-    # Any interesting information about the weather if available
     weather_conditions: str | None = None
 
 
@@ -71,7 +68,6 @@ agent = create_agent(
     checkpointer=checkpointer
 )
 
-# `thread_id` is a unique identifier for a given conversation.
 config = {"configurable": {"thread_id": "1"}}
 
 response = agent.invoke(
@@ -81,13 +77,8 @@ response = agent.invoke(
 )
 
 print(response['structured_response'])
-# ResponseFormat(
-#     punny_response="Florida is still having a 'sun-derful' day! The sunshine is playing 'ray-dio' hits all day long! I'd say it's the perfect weather for some 'solar-bration'! If you were hoping for rain, I'm afraid that idea is all 'washed up' - the forecast remains 'clear-ly' brilliant!",
-#     weather_conditions="It's always sunny in Florida!"
-# )
 
 
-# Note that we can continue the conversation using the same `thread_id`.
 response = agent.invoke(
     {"messages": [{"role": "user", "content": "thank you!"}]},
     config=config,
@@ -95,7 +86,4 @@ response = agent.invoke(
 )
 
 print(response['structured_response'])
-# ResponseFormat(
-#     punny_response="You're 'thund-erfully' welcome! It's always a 'breeze' to help you stay 'current' with the weather. I'm just 'cloud'-ing around waiting to 'shower' you with more forecasts whenever you need them. Have a 'sun-sational' day in the Florida sunshine!",
-#     weather_conditions=None
-# )
+
